e365bb2c292cf674c15af47cd0a35ce4684f2341
anvarknian <ak01011993>
2019-01-28 12:40:16 +0300

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,15 @@
+name := "Problem1"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
+
+version := "1.0"
+
+scalaVersion := "2.12.3"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.0" % Test
+libraryDependencies += "org.slf4j" % "slf4j-api" % "1.7.25"
+libraryDependencies += "org.slf4j" % "slf4j-log4j12" % "1.7.25" % Test
+libraryDependencies += "com.typesafe.scala-logging" %% "scala-logging" % "3.9.0"
+libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.2.3"
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,50 @@
+import java.util.Scanner
+
+import scala.annotation.tailrec
+import scala.util.{Failure, Success, Try}
+
+/**
+  * @author Anwar Knyane
+  */
+
+
+object Main extends App {
+
+  print("-- \t Prime Division \t -- \t Anwar Knyane \t - \t alvinaspowa@gmail.com" +
+    "\n\n\nType a number:")
+
+  val numberInput = Try(new Scanner(System.in).nextLong())
+
+  numberInput match {
+    case Success(a) => print(s"Finding the divider of $a: ${factors(a)}")
+    case Failure(ex) => print("Incorrect type, please enter a positive Long")
+  }
+
+  def factors(n: Long): List[Long] = {
+
+    // Prime divider - 2, 3, 5, 7, 11, 13, 17, 19, ...
+    // We need to find all the prime number smaller than 'a'
+
+    assert(n >= 1, "Argument must be positive")
+
+    @tailrec
+    def primeNumberDivisor(num: Long, list: List[Long]): List[Long] = {
+      if (num == 1) list
+      else {
+        val valueAdd = callRecursionFunction(num)
+        primeNumberDivisor(num / valueAdd, valueAdd :: list)
+      }
+    }
+
+    primeNumberDivisor(n, List()).reverse
+  }
+
+  private def callRecursionFunction(n: Long) = recursionFunction(2, n)
+
+  @tailrec
+  private def recursionFunction(firstPrimeNumber: Long, target: Long): Long = {
+    if (target % firstPrimeNumber == 0) firstPrimeNumber
+    else if (firstPrimeNumber * firstPrimeNumber > target) target
+    else recursionFunction(firstPrimeNumber + 1, target)
+  }
+}
